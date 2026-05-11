4f564eda2fe6f019c0c35066e073bb26e7d21338
tonton1254 <tontonton2356@gmail.com>
2026-05-11 13:19:46 +0545

    added chatbox page

--- app1/src/App.jsx
+++ app1/src/App.jsx
@@ -4,16 +4,19 @@ import Body from "./body";
 import SignUp from "./Form/SignUp";
 
 import Contact from "./contact";
+import ChatBox from "./Chat/ChatBox";
 
 
 function App() {
   return (
     <div>
 
-      <SignUp/>
-      {/* <Header/>
+      {/* <SignUp/>
+      <Header/>
       <Body/>
-      <Contact/> */}
+      <Contact/> */} 
+      <ChatBox></ChatBox>
+
 
     </div>
   );
---
+++ app1/src/Chat/ChatBox.jsx
@@ -0,0 +1,14 @@
+import "./ChatBox.css"
+function ChatBox(){
+
+    return(
+        <div className="Container">
+            <form action="" className="ChatBoxForm">
+                <label htmlFor="">
+                    <input type="text" />
+                </label>
+            </form>
+        </div>
+    );
+}
+export default ChatBox;
